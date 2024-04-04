007e543ff4f6d883ed95db50c106655a5c1f5d3e
Sylvia <sylviachebet03@gmail.com>
2024-04-04 20:20:29 +0300

    add

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ return false
 
 /* 
   Add your pseudocode here
+  create an object to keep track of all the numbers we've seen
+iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  check if any of the keys in our object is the complement to the current number
+    if so, return true
+  save the current number as the key on our object so we can check it later against other numbers
+if we reach the end of the array, return false
 */
 
 /*
